6a4ac79b3122893a04ea0b3d3e44f0518a38e316
Hamdy <hamdysaad.biz@gmail.com>
2023-11-09 18:48:34 +0200

    /fix: type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import React, { FC, ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
